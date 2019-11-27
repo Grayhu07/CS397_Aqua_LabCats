cc0d9b45c045fa30afd25c8e4c09b3bd345016d6
Kechen Hu <buxuxiaowangzi@gmail.com>
2019-11-27 13:58:39 -0600

    add test content

--- material-dashboard-react/cypress/integration/App.spec.js
+++ material-dashboard-react/cypress/integration/App.spec.js
@@ -3,4 +3,9 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it ('test study table', () =>{
+  	cy.visit('/');
+  	cy.get('[data-cy=study]').should('contain', 'taewookim2019@u.northwestern.edu');
+  });
 });
